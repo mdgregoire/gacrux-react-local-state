39abd747c4d70fa08d6ca0a1901cee530685d588
Michael Gregoire <mdgregoire@gmail.com>
2018-03-14 17:04:55 -0500

    single handler

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,8 +6,49 @@ class App extends Component {
 constructor(props) {
   super(props);
 
-  this.state = {user: 'Mike'};
+  this.state = {
+    user: {
+      name: '',
+      city: '',
+    },
+  };
+  //
+  // this.handleName = this.handleName.bind(this);
+  // this.handleCity = this.handleCity.bind(this);
+  // this.handleChangeFor = this.handleChangeFor.bind(this);
+
 }
+
+// handleName(event){
+//   console.log(event.target.value);
+//   this.setState({user: {...this.state.user, name: event.target.value}});
+// }
+//
+// handleCity(event){
+//   console.log(event.target.value);
+//   this.setState({user: {...this.state.user, city: event.target.value}});
+// }
+
+handleChangeFor = (propertyName) => (event) => {
+  this.setState({
+    user:{
+  ...this.state.user,
+  [propertyName]: event.target.value,
+    }
+  })
+}
+
+// handleChangeFor = function (propertyName) {
+//   return function(event){
+//     this.setState({
+//       user: {
+//         ...this.state.user,
+//         [propertyName]: event.target.value
+//       }
+//     })
+//   }
+// }
+
 
   render() {
     return (
@@ -16,7 +57,12 @@ constructor(props) {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p>{this.state.user}</p>
+        <input onChange={this.handleChangeFor('name')} placeholder = "name"/>
+        <input onChange={this.handleChangeFor('city')} placeholder = "city"/>
+        <input onChange={this.handleChangeFor('bowlingScore)')} placeholder = "BowlingScore"/>
+
+        <p>{this.state.user.name} lives in {this.state.user.city}.</p>
+
       </div>
     );
   }
